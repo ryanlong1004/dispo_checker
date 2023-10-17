aea0ee0086f30b9bbdaaba617e0ce6d4f883b1e1
Ryan Long <ryanlong1004@gmail.com>
2023-10-17 00:00:16 +0000

    ignore line-to-long

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long
 """Main point of execution"""
 import json
 import logging
@@ -8,6 +9,7 @@ import requests
 # from src.notifier_text import send_email
 from src.dbb import create_table, find_best_available, get_db_connection, insert_item
 from src.item import Item
+
 
 headers = {
     "content-type": "application/ld+json",
@@ -22,7 +24,6 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
-# pylint: disable=line-too-long
 URL = "https://dutchie.com/graphql?operationName=FilteredProducts&variables=%7B%22includeEnterpriseSpecials%22%3Afalse%2C%22includeCannabinoids%22%3Atrue%2C%22productsFilter%22%3A%7B%22dispensaryId%22%3A%225f07ab86d0b0134ca75fa12b%22%2C%22option%22%3A%221g%22%2C%22pricingType%22%3A%22rec%22%2C%22strainTypes%22%3A%5B%5D%2C%22subcategories%22%3A%5B%5D%2C%22Status%22%3A%22Active%22%2C%22types%22%3A%5B%22Concentrate%22%5D%2C%22useCache%22%3Afalse%2C%22sortDirection%22%3A1%2C%22sortBy%22%3A%22price%22%2C%22isDefaultSort%22%3Afalse%2C%22bypassOnlineThresholds%22%3Afalse%2C%22isKioskMenu%22%3Afalse%2C%22removeProductsBelowOptionThresholds%22%3Atrue%7D%2C%22page%22%3A0%2C%22perPage%22%3A50%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%220e884328c01ef8ed540d4bbd27101ee58fedaf5cddda6c499169209b53fdf574%22%7D%7D"
 
 
